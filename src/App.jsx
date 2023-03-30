48682763299a53ebd22cea24bd5ec4796cf0483c
Adèle Manga <adelemanga75@gmail.com>
2023-03-30 10:38:20 +0200

    i'm adding my exercice on react

--- src/App.jsx
+++ src/App.jsx
@@ -36,6 +36,7 @@ function App() {
 
        figcation="bulbasaur"
        />
+       
     </div>
   )
 }
--- src/src/PokemonCard.jsx
+++ src/src/PokemonCard.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import App from "../App";
 
 export default function PokemonCard() {
+  const pokemon = pokemonList[0];
+  console.log(pokemon)
   return (
-    <div>PokemonCard</div>
-  )
+    <>
+      <div>{pokemon.name}</div>
+
+      {pokemon.imgSrc ? <img src={pokemon.imgSrc}></img> : <p>???</p>}
+    </>
+  );
+  
+
 }
+
+
+
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+
