ec8975bd8baae8876e13026eda57bdeef0be082c
adelemanga <122614418+adelemanga@users.noreply.github.com>
2023-04-05 13:26:44 +0200

    Update App.jsx
    i deleted the comment

--- src/App.jsx
+++ src/App.jsx
@@ -55,41 +55,6 @@ function App() {
       {pokemonIndex< pokemonList.length -1 && <button onClick={hadleNextClick}> Suivant</button>}
       </div>
     }
-      // on doit diminuer pokemonIndex de 1 (le minimum est 0);
-    
-    
-
-      // on doit augmenter pokemonIndex de 1 (le maximum est de pokemonList.length)
-    
-    /*return (
-      <>
-        <button onClick={handlePrevClick}>Précédent</button>
-        <button onClick={handleNextClick}>Suivant</button>
-        <PokemonCard pokemon={pokemonList[pokemonIndex]}></PokemonCard>
-      </>
-    );
-    
-}*/
-
-
-  
-
-
-
-
-    // const [pokemonIndex, setpokemonIndex] = useState({pokemonList});
-    // const handleClick = () => {
-    //   setpokemonIndex (pokemonList + 1)
-    // }
-
-    // return (<div>
-    //            <p>{pokemonIndex.pokemonList}</p>
-    //            <button onClick={handleClick}>Suivant</button>
-    //        </div>);
-
-    // return(<div>
-    //        <p>{setpokemonIndex.pokemonList}</p>
-    //        <button onClick={handleClick}>Précédent</button>
-    //        </div>)
+     
 
 export default App
